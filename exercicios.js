6d6a2f59463b33fd14f997803f29ea8f80e51895
IsabelliSF <isabellisantosfirmino@gmail.com>
2026-09-25 10:36:23 -0300

    atividade aula 3

---
+++ exercicios.js
@@ -0,0 +1,158 @@
+// Exercício 1 - Sistema de Login
+// Crie um objeto usuario com:
+// login
+// senha
+// tipo
+// Depois, exiba a mensagem:
+//O login do administrador é admin e a senha é 1234, muito fraca por sinal.
+
+let usuario = {
+    login: "admin",
+    senha: 1234,
+    tipo: "muito fraca"
+};
+
+console.log(`O login do administrador é ${usuario.login} e a senha é ${usuario.senha}, ${usuario.tipo} por sinal.`);
+
+console.log(`-----------------------------------------------`);
+
+// Exercício 2 – Cadastro de Aluno
+// Crie um objeto aluno com:
+// nome
+// curso
+// turma
+// Depois, exiba a mensagem:
+// O aluno Lucas está matriculado no curso de Desenvolvimento de Sistemas, na turma 2º DS.
+
+let aluno = {
+    nome: "Lucas",
+    curso: "Desenvolvimento de Sistemas",
+    turma: "2ºDS"
+};
+
+console.log(`O aluno ${aluno.nome} está matriculado no curso de ${aluno.curso}, na turma ${aluno.turma}`);
+
+console.log(`-----------------------------------------------`);
+
+// Exercício 3 – Professor
+// Crie um objeto professor com:
+// nome
+// disciplina
+// turno
+// Depois, exiba a mensagem:
+// O professor Carlos ministra a disciplina de JavaScript no turno da manhã.
+
+let professor = {
+    nome: "Carlos",
+    disciplina: "JavaScript",
+    turno: "manhã"
+};
+
+console.log(`O professor ${professor.mome} ministra a disciplina de ${professor.disciplina} no turno da ${professor.turno}.`);
+
+console.log(`-----------------------------------------------`);
+
+// Exercício 4 – Empresa
+// Crie um objeto empresa com:
+// nome
+// area
+// cidade
+// Depois, exiba a mensagem:
+// A empresa Tech Solutions atua na área de Tecnologia e fica em Fortaleza.
+
+let empresa = {
+    nome: "Tech Solutions",
+    area: "Tecnologia",
+    cidade: "Fortaleza"
+};
+
+console.log(`A empresa ${empresa.nome} atua na ádea de ${empresa.area} e fica em ${empresa.cidade}.`)
+
+console.log(`-----------------------------------------------`);
+
+// Exercício 5 – Computador
+// Crie um objeto computador com:
+// marca
+// processador
+// memoria
+// Depois, exiba a mensagem:
+// O computador da marca Dell possui processador Intel i5 e memória de 8GB.
+
+let computador = {
+    marca: "Dell",
+    processador: "Intel i5",
+    memoria: "8GB"
+};
+
+console.log(`O computador da marca ${computador.marca} possui processador ${computador.processador} e memória de ${computador.memoria}`);
+
+console.log(`-----------------------------------------------`);
+
+// Exercício 6 – Livro
+// Crie um objeto livro com:
+// titulo
+// autor
+// paginas
+// Depois, exiba a mensagem:
+// O livro JavaScript Básico foi escrito por Ana Silva e possui 200 páginas.
+
+let livro = {
+    titulo: "JavaScript Básico",
+    autor: "Ana Silva",
+    paginas: 200
+};
+
+console.log (`O livro ${livro.titulo} foi escrito por ${livro.autor} e possui ${livro.paginas} páginas.`);
+
+console.log(`-----------------------------------------------`);
+
+// Exercício 7 – Filme
+// Crie um objeto filme com:
+// nome
+// genero
+// ano
+// Depois, exiba a mensagem:
+// O filme Matrix é do gênero Ficção e foi lançado em 1999.
+
+let filme = {
+    nome: "Matrix",
+    genero: "Ficção",
+    ano: 1999
+};
+
+console.log(`O filme ${filme.nome} é do gênero ${filme.genero} e foi lançado em ${filme.ano}.`);
+
+console.log(`-----------------------------------------------`);
+
+// Exercício 8 – Jogo
+// Crie um objeto jogo com:
+// nome
+// plataforma
+// genero
+// Depois, exiba a mensagem:
+// O jogo Minecraft roda na plataforma PC e pertence ao gênero Sandbox.
+
+let jogo ={
+    nome: "Minecraft",
+    plataforma: "PC",
+    genero: "Sandbox"
+};
+
+console.log(`O jogo ${jogo.nome} roda na plataforma ${jogo.plataforma} e pertence ao gênero ${jogo.genero}.`);
+
+console.log(`-----------------------------------------------`);
+
+// Exercício 10 – Escola
+// Crie um objeto escola com:
+// nome
+// cidade
+// turno
+// Depois, exiba a mensagem:
+// A escola EEEP funciona na cidade de Fortaleza no turno da manhã.
+
+let escola = {
+    nome: "EEEP",
+    cidade: "Fortaleza",
+    turno: "manhã"
+};
+
